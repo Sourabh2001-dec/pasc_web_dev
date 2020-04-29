42d5c93514267ee407544889d91838dd74af60fa
Sourabh Kumbhar <55979623+Sourabh2001-dec@users.noreply.github.com>
2020-04-29 10:16:52 +0530

    cape animation

---
+++ index.js
@@ -0,0 +1,15 @@
+anime({
+    targets : '#path5530',
+    translateX : '20px',
+    direction : 'alternate',
+    duration : 2000,
+    loop : true,
+    easing : 'linear'
+})
+// anime({
+//     targets : '#path5594',
+//     translateX : '20px',
+//     direction : 'alternate',
+//     duration : 2000,
+//     loop : true
+// })
