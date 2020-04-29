f9beda5d1b25c4f64dc9af3a2065af7f5e9ee0e9
Sourabh Kumbhar <55979623+Sourabh2001-dec@users.noreply.github.com>
2020-04-29 15:52:50 +0530

    started about corona

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 anime({
     targets : '#path5530',
     translateX : '20px',
+    scale : [1,1.04],
     direction : 'alternate',
-    duration : 2000,
+    duration : 1500,
     loop : true,
     easing : 'linear'
 })
