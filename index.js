d7ca7a02f63097d89c12a2ac5fd4f643e536f39c
Sourabh Kumbhar <55979623+Sourabh2001-dec@users.noreply.github.com>
2020-05-04 22:47:23 +0530

    charts added

--- index.js
+++ index.js
@@ -6,14 +6,7 @@ $(window).scroll(function() {
     }
   });
 
-  $(function () {
-			
-    // $.scrollify({
-    //     section: 'section',
-    // });
 
-    
-});
 
 anime({
     targets : '#path5530',
@@ -37,6 +30,151 @@ anime({
     loop : true,
     duration : 5000
 })
+state_codes = {
+  TT : {
+    name : "All States",
+    Confirmed : [],
+        Recovered : [],
+        Deceased : [],
+        Confirmed_date : [],
+        Recovered_date : [],
+        Deceased_date : [],
+  }
+}
+    chart_config = {
+      type : 'line',
+      data : {
+        labels : [],
+        datasets : [
+          {
+            data : [],
+            borderColor: "red",
+            label : 'No. of cases',
+            pointRadius : 2,
+            fill: false
+          }
+        ]
+      },
+      options : {
+        title : {
+          display : true,
+          text : 'All States confirmed COVID-19 cases'
+        }
+      }
+    }
+
+
+myChart = null
+const state_select = document.getElementById('state_select')
+const case_select = document.getElementById('case_type')
+let graph_state = {
+  stateCode : "TT",
+  caseType : "Confirmed"
+}
+state_select.addEventListener('change',(e)=>{
+  const code = state_select.options[state_select.selectedIndex].getAttribute('value')
+  graph_state.stateCode = code
+  addData(state_codes[code],graph_state.caseType)
+})
+
+case_select.addEventListener('change',()=>{
+  const type = case_select.options[case_select.selectedIndex].getAttribute('value')
+  graph_state.caseType = type
+  addData(state_codes[graph_state.stateCode],type)
+})
+
+fetch('https://api.covid19india.org/v2/state_district_wise.json')
+  .then(response => response.json())
+  .then(data => {
+    data.forEach(state => {
+      state_codes[state.statecode] = {name : state.state,
+        Confirmed : [],
+        Recovered : [],
+        Deceased : [],
+        Confirmed_date : [],
+        Recovered_date : [],
+        Deceased_date : [],
+
+      }  
+    });
+
+    
+  }).then(
+
+  fetch('https://api.covid19india.org/states_daily.json')
+  .then(response => response.json())
+  .then(data => {
+  
+    data.states_daily.forEach(info => {
+      Object.keys(info).forEach(function(key){
+        if(key !== "status" || key!== "date" ){
+        capKey = key.toUpperCase()
+        status = info.status
+        state = state_codes[capKey]
+        if(state){
+          length = state[info.status].length
+          length > 0 ? new_num = state[info.status][ length -1] + parseInt(info[key]) : new_num = parseInt(info[key])
+          state[info.status].push(new_num)
+          let date = info['date'].split("-")
+          date = date[0] +" "+ date[1]
+          state[info.status+'_date'].push(date)
+          if (capKey !== "TT") {
+            
+            state_select.insertAdjacentHTML('beforeend',`<option value=${capKey}>${state.name}</option>`)
+          }
+        }
+        }
+       
+      })
+    });
+
+
+
+    
+    addData(state_codes[graph_state.stateCode],graph_state.caseType,true)
+    var ctx = document.getElementById('myChart').getContext('2d');
+    myChart = new Chart(ctx, chart_config)
+  })
+  )
+
+  function addData(data,type,start = false) {
+  
+    chart_config.options.title.text = data.name+" "+type+" COVID-19 cases"
+    chart_config.data.datasets.forEach((dataset) => {
+        dataset.data = data[type];
+    });
+    chart_config.data.labels = data[type+"_date"]
+    if (type == "Recovered") {
+      chart_config.data.datasets.forEach((dataset) => {
+        dataset.borderColor = "green";
+    });
+    }
+    else{
+      chart_config.data.datasets.forEach((dataset) => {
+        dataset.borderColor = "red";
+    });
+    }
+   if(!start){
+
+     myChart.update();
+   }
+
+    document.getElementById("state_name").innerText = state_codes[graph_state.stateCode].name
+    document.getElementById("state_conf").innerText = state_codes[graph_state.stateCode].Confirmed[state_codes[graph_state.stateCode].Confirmed.length - 1]
+    document.getElementById("state_reco").innerText = state_codes[graph_state.stateCode].Recovered[state_codes[graph_state.stateCode].Recovered.length - 1]
+}
+
+function removeData() {
+    // chart.data.labels.pop();
+    chart_config.data.datasets.forEach((dataset) => {
+        dataset.data.pop();
+    });
+    myChart.update();
+}
+    
+
+
+
 
 // anime({
 //     targets : '#path5594',
